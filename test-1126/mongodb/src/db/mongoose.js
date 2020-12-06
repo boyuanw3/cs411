95d9d649be1560ec7f001537d3e11a9bc26050f5
Boyuan Wang <boyuanwang@Boyuan-Wang-MacBook-Pro.local>
2020-12-05 21:49:06 -0600

    final

--- test-1126/mongodb/src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose =  require('mongoose')
-const User = require('../model/user')
-
-mongoose.connect('mongodb://127.0.0.1:27017/WikiMusic-user', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
-
